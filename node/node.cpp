7685895ca506abe05255bda0f3bc1d015a87553e
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-02-14 09:57:14 +0100

    Updated node.cpp

--- node/node.cpp
+++ node/node.cpp
@@ -3,21 +3,43 @@ namespace node {
     unsigned int sendDatas(string datas) {
         
         string receiversResponses = [];
+        unsigned long long transmissionErrors = 0;
+        
+        for (unsigned long long i; i < receiversResponses.length; i++) {
+            
+            if (receiversResponses[i[0 : 4]] == "error") {
+                
+                transmissionErrors += 1;
+                std::cout << "Error when transmitting some datas to the nodes connected !" << std::endl;
+                
+            };
+            
+        };
         
     };
     unsigned int sendDatasTo(string toAddress, unsigned int toPort, string datas) {
         
         string receiverResponse;
+        unsigned long long transmissionErrors = 0;
+        
+        if (receiverResponse[0 : 4] == "error") {
+            
+            transmissionErrors += 1;
+            std::cout << "Error when tranmitting some datas to the node " << toAddress << " : " << toPort << " !" << std::endl;
+            
+        };
         
     };
     unsigned int receiveDatas() {
         
         string sendersDatas = [];
+        unsigned long long transmissionErrors = 0;
         
     };
     unsigned int receiveDatasFrom(string fromAddress, string fromPort) {
         
         string senderDatas;
+        unsigned long long transmissionErrors = 0;
         
     };
     
