59924824bf91be6a9ba3f34e1d512086987fea81
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-02-14 09:03:52 +0100

    Created blockchain.cpp

---
+++ blockchain/blockchain.cpp
@@ -0,0 +1,53 @@
+#include <fs>
+#include <iostream>
+#include <string>
+
+#include "../wallet/wallet.cpp"
+#include "../transaction/transaction.cpp"
+
+namespace blockchain {
+    
+    struct Block {
+        
+        string blockId,
+               blockHexUnixDate,
+               blockTransactions;
+        
+    };
+    
+    string lastBlockContent = "",
+           lastBlockHexUnixDate,
+           lastBlockSha512Hash = "";
+    
+    unsigned int createBlock() {
+        
+        
+        
+    };
+    unsigned int getBlock(string blockId) {
+        
+        
+        
+    };
+    unsigned int signBlock(Block BLOCK, wallet.privateKey PRIVATEKEY) {
+        
+        
+        
+    };
+    unsigned int verifyBlock(Block BLOCK) {
+        
+        
+        
+    };
+    unsigned int confirmBlock(Block BLOCK) {
+        
+        
+        
+    };
+    unsigned int relayBlock(Block BLOCK, transaction.signature) { // relaying a specific block to 
+        
+        
+        
+    };
+    
+}
