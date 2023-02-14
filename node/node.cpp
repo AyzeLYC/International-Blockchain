03af6ed762dcde5c26c5c458c9354279cb832143
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-02-14 09:44:15 +0100

    Created node.cpp

---
+++ node/node.cpp
@@ -0,0 +1,24 @@
+namespace node {
+    
+    unsigned int sendDatas(string datas) {
+        
+        string receiversResponses = [];
+        
+    };
+    unsigned int sendDatasTo(string toAddress, unsigned int toPort, string datas) {
+        
+        string receiverResponse;
+        
+    };
+    unsigned int receiveDatas() {
+        
+        string sendersDatas = [];
+        
+    };
+    unsigned int receiveDatasFrom(string fromAddress, string fromPort) {
+        
+        string senderDatas;
+        
+    };
+    
+}
