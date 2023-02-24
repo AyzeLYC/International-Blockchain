a44bf0894297da2249693130406c5dc30081a8bd
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-02-24 15:38:55 +0100

    Created main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <std>
+#include <string>
+
+#include "blockchain/blockchain.cpp"
+#include "cli/cli.cpp"
+#include "derivation/derivation.cpp"
+#include "hashing/hashing.cpp"
+#include "node/node.cpp"
+#include "smart-contract/smart-contract.cpp"
+#include "transaction/transaction.cpp"
+#include "ui/ui.cpp"
+#include "wallet/wallet.cpp"
+
+namespace main {
+    
+    
+    
+}
