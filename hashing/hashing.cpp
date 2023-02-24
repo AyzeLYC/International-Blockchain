7d159bf82c06f25e1eb37e77de5adcba2e4665c2
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-02-24 10:17:21 +0100

    Updated hashing.cpp

--- hashing/hashing.cpp
+++ hashing/hashing.cpp
@@ -1,8 +1,103 @@
-#include "sha512.cpp"
+#include <string>
 
 namespace hashing {
     
-    unsigned int hash() {
+    unsigned int argon2(string datas) {
+        
+        
+        
+    };
+    unsigned int c11(string datas) {
+        
+        
+        
+    };
+    unsigned int power2b(string datas) {
+        
+        
+        
+    };
+    unsigned int scrypt(string datas) {
+        
+        
+        
+    };
+    unsigned int sha256(string datas) {
+        
+        
+        
+    };
+    unsigned int sha256d(string datas) {
+        
+        
+        
+    };
+    unsigned int sha256t(string datas) {
+        
+        
+        
+    };
+    unsigned int sha256q(string datas) {
+        
+        
+        
+    };
+    unsigned int sha512(string datas) {
+        
+        
+        
+    };
+    unsigned int sha512d(string datas) {
+        
+        
+        
+    };
+    unsigned int sha512t(string datas) {
+        
+        
+        
+    };
+    unsigned int sha512q(string datas) {
+        
+        
+        
+    };
+    unsigned int vanilla(string datas) {
+        
+        
+        
+    };
+    unsigned int x11(string datas) {
+        
+        
+        
+    };
+    unsigned int x12(string datas) {
+        
+        
+        
+    };
+    unsigned int x13(string datas) {
+        
+        
+        
+    };
+    unsigned int x14(string datas) {
+        
+        
+        
+    };
+    unsigned int x15(string datas) {
+        
+        
+        
+    };
+    unsigned int x16r(string datas) {
+        
+        
+        
+    };
+    unsigned int x16s(string datas) {
         
         
         
