39d076d7c3549efbf3f63447627b27c131c41911
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-02-28 20:25:44 +0100

    Created internet.cpp

---
+++ internet/internet.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+namespace internet {
+    
+    unsigned int createUdpServer(string address, unsigned int port) {
+        
+        
+        
+    };
+    unsigned int createTcpServer(int domain, int type, string address, unsigned int port) {
+        
+        
+        
+    };
+    
+};
