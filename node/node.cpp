baa0e373b6ebe8992965fc07748b76ad12729d58
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-08 10:53:43 +0100

    Updated node.cpp

--- node/node.cpp
+++ node/node.cpp
@@ -5,17 +5,19 @@
 
 namespace node {
     
+    uint32_t transmissionErrors = 0,
+             receptionErrors = 0;
+    
     unsigned int sendDatas(string datas) {
         
         string receiversResponses = [];
-        uint32_t transmissionErrors = 0;
         
         internet.blockchainSocket.send(datas);
         receiversResponses = internet.blockchainSocket.recv();
         
         for (uint32_t i; i < receiversResponses.length; i++) {
             
-            if (receiversResponses[i] <= 0 || receiversResponses[i[0 : 4]] == "error") {
+            if (receiversResponses[i] < 0 || receiversResponses[i[0 : 4]] == "error") {
                 
                 transmissionErrors += 1;
                 std::cout << "Error when transmitting some datas to one of the nodes connected !" << std::endl;
@@ -28,16 +30,21 @@ namespace node {
     unsigned int sendDatasTo(string toAddress, uint toPort, string datas) {
         
         string receiverResponse;
-        unsigned long long transmissionErrors = 0;
         
-        connect(internet.blockchainSocket, [toAddress, toPort], sizeof([toAddresss, toPort]));
+        internet.blockchainSocket.connect([toAddress, toPort], sizeof([toAddresss, toPort]));
         internet.blockchainSocket.send(datas);
         receiverResponse = internet.blockchainSocket.recv();
+        internet.blockchainSocket.close();
         
-        if (receiverResponse <= 0 || receiverResponse[0 : 4] == "error") {
+        if (receiverResponse < 0 || receiverResponse[0 : 4] == "error") {
             
-            transmissionErrors += 1;
+            trasmissionErrors += 1;
             std::cout << "Error when transmitting some datas to the node " << toAddress << " : " << toPort << " !" << std::endl;
+            return 0;
+            
+        } else {
+            
+            return receiverResponse;
             
         };
         
@@ -47,13 +54,40 @@ namespace node {
         string sendersDatas = [];
         uint32_t transmissionErrors = 0;
         
+        sendersDatas = internet.blockchainSocket.recv();
         
+        while (uint32_t i; i < sendersDatas.length; i++) {
+            
+            if (senderDatas[i] < 0 || senderDatas[0 : 4] == "error") {
+                
+                receptionErrors += 1;
+                std::cout << "An error occured while receiving the datas !" << std::endl;
+                sendersDatas[i] = 0;
+                
+            };
+            
+        }
+        
+        return sendersDatas;
         
     };
     unsigned int receiveDatasFrom(string fromAddress, string fromPort) {
         
         string senderDatas;
-        unsigned long long transmissionErrors = 0;
+        
+        senderDatas = internet.blockchainSocket.recv();
+        
+        if (senderDatas < 0 || senderDatas[0 : 4] == "error") {
+            
+            receptionErrors += 1;
+            std::cout << "Error while receiving some datas from " << fromAddress <<" !" << std::endl;
+            return 0;
+            
+        } else {
+            
+            return senderDatas;
+            
+        };
         
     };
     
