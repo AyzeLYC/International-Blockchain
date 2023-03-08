eb9f0c48f562a38eb99672326dd0310403540527
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-08 10:25:55 +0100

    Updated node.cpp

--- node/node.cpp
+++ node/node.cpp
@@ -8,14 +8,14 @@ namespace node {
     unsigned int sendDatas(string datas) {
         
         string receiversResponses = [];
-        uint256_t transmissionErrors = 0;
+        uint32_t transmissionErrors = 0;
         
         internet.blockchainSocket.send(datas);
         receiversResponses = internet.blockchainSocket.recv();
         
-        for (uint256_t i; i < receiversResponses.length; i++) {
+        for (uint32_t i; i < receiversResponses.length; i++) {
             
-            if (receiversResponses[i] > 0 || receiversResponses[i[0 : 4]] == "error") {
+            if (receiversResponses[i] <= 0 || receiversResponses[i[0 : 4]] == "error") {
                 
                 transmissionErrors += 1;
                 std::cout << "Error when transmitting some datas to one of the nodes connected !" << std::endl;
@@ -25,12 +25,16 @@ namespace node {
         };
         
     };
-    unsigned int sendDatasTo(string toAddress, unsigned int toPort, string datas) {
+    unsigned int sendDatasTo(string toAddress, uint toPort, string datas) {
         
         string receiverResponse;
         unsigned long long transmissionErrors = 0;
         
-        if (receiverResponse[0 : 4] == "error") {
+        connect(internet.blockchainSocket, [toAddress, toPort], sizeof([toAddresss, toPort]));
+        internet.blockchainSocket.send(datas);
+        receiverResponse = internet.blockchainSocket.recv();
+        
+        if (receiverResponse <= 0 || receiverResponse[0 : 4] == "error") {
             
             transmissionErrors += 1;
             std::cout << "Error when transmitting some datas to the node " << toAddress << " : " << toPort << " !" << std::endl;
@@ -41,7 +45,9 @@ namespace node {
     unsigned int receiveDatas() {
         
         string sendersDatas = [];
-        unsigned long long transmissionErrors = 0;
+        uint32_t transmissionErrors = 0;
+        
+        
         
     };
     unsigned int receiveDatasFrom(string fromAddress, string fromPort) {
