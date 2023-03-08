10b03eae840de8a889cfaf37953571da39a6a48c
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-08 15:00:21 +0100

    Updated AES-256-CBC.cpp

--- encryption/AES-256-CBC.cpp
+++ encryption/AES-256-CBC.cpp
@@ -1,5 +1,14 @@
 namespace AES256CBC {
     
-    
+    unsigned int encrypt(string DATAS, string password) {
+        
+        
+        
+    };
+    unsigned int decrypt(string DATAS, string password) {
+        
+        
+        
+    };
     
 }
