e021c071a845af090d3771bd0df0c349d63f1c70
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-08 15:31:15 +0100

    Created encryption.cpp

---
+++ encryption/encryption.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include "AES-256-CBC/AES-256-CBC.cpp"
+
+namespace encryption {
+    
+    unsigned int AES256CBC(string datas, string password) {
+        
+        
+        
+    };
+    
+}
