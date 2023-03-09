be78ef3075cebf34590de2dfd3d2041cee802cac
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-09 15:01:02 +0100

    Updated messaging.cpp

--- messaging/messaging.cpp
+++ messaging/messaging.cpp
@@ -1,8 +1,22 @@
 #include <string>
 #include "../encryption/encryption.cpp"
+#include "wallet/wallet.cpp"
 
 namespace messaging {
     
+    struct Message {
+        
+        string message,
+               publicKey,
+               signature;
+        
+    }
     
+    unsigned int createMessage(string message, wallet.privateKey)
+    unsigned int sendMessage(Message MESSAGE, uint8_t address[4], unsigned int port) {
+        
+        
+        
+    };
     
 }
