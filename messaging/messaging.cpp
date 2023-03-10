eef193abac9dacf72a4914a66b077f261ef39bc2
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-10 08:23:14 +0100

    Updated messaging.cpp

--- messaging/messaging.cpp
+++ messaging/messaging.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include "../derivation/derivation.cpp"
 #include "../encryption/encryption.cpp"
 #include "../node/node.cpp"
 #include "../wallet/wallet.cpp"
@@ -7,31 +8,59 @@ namespace messaging {
     
     struct Message {
         
-        string publicKey,
+        string senderPublicKey,
+               messageSignature,
                message;
         
     }
     
     unsigned int signMessage(wallet.privateKey PRIVATEKEY, wallet.publicKey PUBLICKEY, string MESSAGE) {
         
-        string signedMessage = new Message(PUBLICKEY, wallet.sign(message, PRIVATEKEY));
+        string encryptedMessage = new Message(PUBLICKEY, wallet.sign(message, PRIVATEKEY));
         
-        return signedMessage;
+        return encryptedMessage;
+        
+    };
+    unsigned int verifyMessage(Message MESSAGE) {
+        
+        bool messageValid;
+        
+        if (wallet.verify(message[64 : 127], message[0 : 31])) {
+            
+            messageValid = true;
+            
+        } else {
+            
+            messageValid = false;
+            
+        };
+        
+        return messageValid;
         
     };
     unsigned int sendMessageToBlockchainNode(Message MESSAGE, string address, unsigned int port) {
         
-        bool response = node.sendDatasTo(address, port, MESSAGE);
+        string response;
+        bool responseValid;
         
-        if (response == false) {
+        if (response < 0 || response[0 : 4] == "error") {
             
             std::cout << "Error while sending the message to the node " << address << ":" << port << " !" << std::endl;
+            responseValid = false;
             
         } else {
             
-            std::cout << "The message has been sent !" << std::endl; 
+            std::cout << "The message has been sent !" << std::endl;
+            responseValid = true;
             
         };
+        
+        return responseValid;
+        
+    };
+    unsigned int sendMessageToBlockchainNodes(Message MESSAGE) {
+        
+        
         
     };
     
