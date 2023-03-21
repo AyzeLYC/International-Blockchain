decdd3a464eb8b50530e699a17ecd0fda3c13646
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-21 10:19:50 +0100

    Updated ui.cpp

--- ui/ui.cpp
+++ ui/ui.cpp
@@ -2,6 +2,51 @@
 
 namespace ui {
     
+    string applicationPages = {};
     
+    unsigned int createApplication(string title, string xSize, string ySize, uint24_t backgroundRGBColor, uint16_t backgroundOpacity) {
+        
+        
+        
+    };
+    
+    unsigned int startApplication() {
+        
+        
+        
+    };
+    unsigned int stopApplication() {
+        
+        
+        
+    };
+    
+    
+    unsigned int createPage(string title, string xSize, string ySize, uint24_t backgroundRGBColor, uint16_t backgroundOpacity) {
+        
+        
+        
+    };
+    unsigned int modifyPage(string title, string xSize, string ySize, uint24_t backgroundRGBColor, uint16_t backgroundOpacity) {
+        
+        
+        
+    };
+    unsigned int deletePage() {
+        
+        
+        
+    };
+    
+    unsigned int showPage() {
+        
+        
+        
+    };
+    unsigned int hidePage() {
+        
+        
+        
+    };
     
 }
