00c1c70f0f8d7a646585b820b24cc0757f955bd1
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-30 09:54:48 +0200

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@
 #include "blockchain/blockchain.cpp"
 #include "cli/cli.cpp"
 #include "derivation/derivation.cpp"
+#include "encryption/encryption.cpp"
 #include "hashing/hashing.cpp"
+#include "internet/internet.cpp"
+#include "messaging/messaging.cpp"
 #include "node/node.cpp"
 #include "smart-contract/smart-contract.cpp"
 #include "transaction/transaction.cpp"
@@ -14,6 +17,28 @@
 
 namespace main {
     
-    
+    unsigned int start() {
+        
+        ui.start();
+        wallet.start();
+        blockchain.start();
+        
+    };
+    unsigned int pause() {
+        
+        messaging.pause();
+        internet.pause();
+        smartcontract.pause();
+        transaction.pause();
+        
+    };
+    unsigned int stop() {
+        
+        messaging.stop();
+        internet.stop();
+        smartcontract.stop();
+        transaction.stop();
+        
+    };
     
 }
